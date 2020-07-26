81c227c080ca052bc94c1adf9413dd12b6f653ec
Lijie Ma <ljma@thoughtworks.com>
2020-07-26 17:24:14 +0800

    learn basic file structure,how kotlin interact with layouts

--- app/src/main/java/org/actnow/at/tmapp/FirstFragment.kt
+++ app/src/main/java/org/actnow/at/tmapp/FirstFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import kotlin.random.Random
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,8 +17,8 @@ import androidx.navigation.fragment.findNavController
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
@@ -25,7 +28,24 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val myToast = Toast.makeText(context, "Hello Toast!", Toast.LENGTH_SHORT)
+            myToast.show()
         }
+
+        view.findViewById<Button>(R.id.button_first2).setOnClickListener {
+            countMe(view)
+        }
+
+        view.findViewById<Button>(R.id.button_first3).setOnClickListener {
+            view.findViewById<TextView>(R.id.textview_first).text = Random.nextInt().toString();
+        }
+    }
+
+    fun countMe(view: View) {
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+        val countString = showCountTextView.text.toString()
+        var count = countString.toInt()
+        count++
+        showCountTextView.text = count.toString()
     }
 }
